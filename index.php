7332034065c98e2cd7ed663e8526c64080b8a3a0
faq322 <34307377+faq322@users.noreply.github.com>
2020-10-09 18:39:43 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$dbname = "socialnetwork";
+$tablename = "test";
+$con = mysqli_connect("localhost", "root", "", $dbname);
+
+if (mysqli_connect_errno()) {
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES(NULL, 'Igor')");
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Social Network</title>
+</head>
+<body>
+Hello igor!s
+</body>
+</html>
