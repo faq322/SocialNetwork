8f2ed0ac6efc38d38b1cc8f48419e0ecdfe679d0
faq322 <34307377+faq322@users.noreply.github.com>
2020-10-09 19:47:08 +0300

    generating a unique username

--- register.php
+++ register.php
@@ -96,6 +96,23 @@ if (isset($_POST['register_button'])){
 		array_push($error_array, "Your password must be between 5 and 30 chars<br>");
 	}
 
+	if(empty($error_array)){
+		$password = md5($password); //Encrypts password
+
+		//Generate username by concatenating first name and last name
+		$username = strtolower($fname . "_" . $lname);
+		$check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+
+		$i = 0;
+		//if username exists add number to username
+		while(mysqli_num_rows($check_username_query) != 0) {
+			$i++;
+			$username = $username . "+" . $i;
+			$check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
+		}
+	}
+
+
 
 }
 
